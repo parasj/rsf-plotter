24a5baf045f01710887038fd5e81882594abe209
Paras Jain <parasj96@gmail.com>
2022-10-31 18:48:51 -0700

    Add spinner

--- plot_rsf.py
+++ plot_rsf.py
@@ -9,31 +9,34 @@ import matplotlib.dates as mdates
 
 plt.style.use('ggplot')
 
-truncate_months = st.sidebar.slider('Truncate months', 0, 12, 3)
-start = time.time()
-data_url = "https://people.eecs.berkeley.edu/~paras/rsf_occupancy.jsonl"
-with urllib.request.urlopen(data_url) as f:
-    jsonl = f.read().decode("utf-8").strip().split("\n")
-    n_records = 60 * 24 * 30 * truncate_months
-    records = [json.loads(line) for line in jsonl[-n_records:]]
-    df = pd.DataFrame(records)
-end = time.time()
+truncate_months = st.slider('Truncate months', 0, 12, 3)
+
+with st.spinner("Loading data..."):
+    start = time.time()
+    data_url = "https://people.eecs.berkeley.edu/~paras/rsf_occupancy.jsonl"
+    with urllib.request.urlopen(data_url) as f:
+        jsonl = f.read().decode("utf-8").strip().split("\n")
+        n_records = 60 * 24 * 30 * truncate_months
+        records = [json.loads(line) for line in jsonl[-n_records:]]
+        df = pd.DataFrame(records)
+    end = time.time()
 print(f"loaded in {end - start:.2f} seconds")
 
 @st.cache
 def map_date(date):
     return pd.to_datetime(date, format="%Y-%b-%d %H:%M:%S", cache=True)
 
-start = time.time()
-parser = re.compile(r"(?P<day_of_week>\w+) (?P<month>\w+) (?P<day>\d+) (?P<hour>\d+):(?P<minute>\d+):(?P<second>\d+) (?P<timezone>\w+) (?P<year>\d+)")
-parsed = df["datetime"].apply(lambda x: parser.match(x).groupdict())
-# filter date to current year and month or last month
-df["datetime_str"] = parsed.apply(lambda x: f"{x['year']}-{x['month']}-{x['day']} {x['hour']}:{x['minute']}:{x['second']}")
-df["datetime"] = df["datetime_str"].apply(map_date)
-df = df.drop(columns=["datetime_str"])
-df["date"] = df["datetime"].dt.date
-end = time.time()
-print(f"parsed in {end - start:.2f} seconds")
+with st.spinner("Parsing dates..."):
+    start = time.time()
+    parser = re.compile(r"(?P<day_of_week>\w+) (?P<month>\w+) (?P<day>\d+) (?P<hour>\d+):(?P<minute>\d+):(?P<second>\d+) (?P<timezone>\w+) (?P<year>\d+)")
+    parsed = df["datetime"].apply(lambda x: parser.match(x).groupdict())
+    # filter date to current year and month or last month
+    df["datetime_str"] = parsed.apply(lambda x: f"{x['year']}-{x['month']}-{x['day']} {x['hour']}:{x['minute']}:{x['second']}")
+    df["datetime"] = df["datetime_str"].apply(map_date)
+    df = df.drop(columns=["datetime_str"])
+    df["date"] = df["datetime"].dt.date
+    end = time.time()
+    print(f"parsed in {end - start:.2f} seconds")
 
 # show detailed data for today since 7am
 today = pd.Timestamp.today()
